52f1207aa38fd3b1ee35a8f9a4d706ab836f5643
VM39 <vgsvpmahesh@gmail.com>
2026-07-31 16:40:35 +0530

    added variable.tf adn output.tf made some changes to the report.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,4 @@
 resource "local_file" "deployment_record" {
-  filename = "${path.module}/deployment-record.txt"
-  content  = <<-EOF
-    Deployment Record
-    ------------------
-    Managed by: Terraform
-    Purpose: Infrastructure validation placeholder
-  EOF
+  filename = "deployment-record-${var.environment}.txt"
+  content  = "Deployed to: ${var.environment}"
 }
